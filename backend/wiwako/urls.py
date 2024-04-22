4e20dd8a830c8ac4ab7b9b8779bf338cfbdf99c0
luka <102388689+F1R0o@users.noreply.github.com>
2024-04-22 21:54:14 +0400

    asdasd
    axali

---
+++ backend/wiwako/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path
+from .views import HomeAPIView, ProductPageAPIView, SearchResultsAPIView,CarouselApi,WiwakoDetailAPIView
+
+urlpatterns = [
+     path('home/', HomeAPIView.as_view(), name='home_api'),
+     path('products/', ProductPageAPIView.as_view(), name='product_page_api'),
+     path('search/', SearchResultsAPIView.as_view(), name='search_results_api'),
+     path("carousel/",CarouselApi.as_view(),name="CAROUSEL"),
+     path("products/<int:id>/",WiwakoDetailAPIView.as_view(),name='DETAIL')
+]
+
+
+
+if settings.DEBUG:
+       urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
